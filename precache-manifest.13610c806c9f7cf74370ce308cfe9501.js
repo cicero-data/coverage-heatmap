dc9a828d6a24eb0ca40f0b2ae61b43ab6e99df4e
Luke McKinstry <lukemckinstry@gmail.com>
2021-01-27 15:28:26 -0500

    Updates

---
+++ precache-manifest.13610c806c9f7cf74370ce308cfe9501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3618440a8961b12123e3a350e3cfc7f7",
+    "url": "/coverage-heatmap/index.html"
+  },
+  {
+    "revision": "317b501703f1ed46cc9d",
+    "url": "/coverage-heatmap/static/css/main.ebc3e4f7.chunk.css"
+  },
+  {
+    "revision": "2bd5a9d71afcd4eb5143",
+    "url": "/coverage-heatmap/static/js/2.5bc1b99f.chunk.js"
+  },
+  {
+    "revision": "22541dec0ae3a5c907b2f23bbb906219",
+    "url": "/coverage-heatmap/static/js/2.5bc1b99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317b501703f1ed46cc9d",
+    "url": "/coverage-heatmap/static/js/main.d5c8e6b0.chunk.js"
+  },
+  {
+    "revision": "fb37fb2c8b6a941feec2",
+    "url": "/coverage-heatmap/static/js/runtime-main.ef8a1b0a.js"
+  },
+  {
+    "revision": "039e4058cbb0631e8ed4caa7f007422e",
+    "url": "/coverage-heatmap/static/media/cicero_white_tagline.039e4058.svg"
+  }
+]);
